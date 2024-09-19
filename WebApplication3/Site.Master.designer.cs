e31ee1972f412c016c5bcae457732224d0367a34
amandeep-rayat <118011469+amandeep-rayat@users.noreply.github.com>
2024-09-19 15:42:12 +0530

    minor fixes

--- WebApplication3/Site.Master.designer.cs
+++ WebApplication3/Site.Master.designer.cs
@@ -13,7 +13,24 @@ namespace WebApplication3
 
     public partial class SiteMaster
     {
-
+        /// <summary>
+        /// LogOut control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button LogOut;
+        
+        /// <summary>
+        /// confirmLogout control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button confirmLogout;
+        
         /// <summary>
         /// MainContent control.
         /// </summary>
